53d9797056b8a87a0ffec2ff1ebedd3e01c0eb88
Kaik <kapitanogor@googlemail.com>
2017-12-26 17:17:18 +0100

    Add import controller

---
+++ Controller/ImportController.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * KaikMedia PagesModule
+ *
+ * @package    KaikmediaPagesModule
+ * @author     Kaik <contact@kaikmedia.com>
+ * @copyright  KaikMedia
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @link       https://github.com/Kaik/KaikMediaPages.git
+ */
+
+namespace Kaikmedia\PagesModule\Controller;
+
+use Kaikmedia\PagesModule\Form\Type\SettingsType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Zikula\Core\Controller\AbstractController;
+use Zikula\ThemeModule\Engine\Annotation\Theme;
+
+/**
+ * @Route("/import")
+ */
+class ImportController extends AbstractController
+{
+    /**
+     * @Route("/index")
+     *
+     * @Theme("admin")
+     *
+     * the main administration function
+     *
+     * @return RedirectResponse
+     */
+    public function indexAction()
+    {
+        // access throw component instance user
+        $this->get('kaikmedia_pages_module.access_manager')->hasPermission(ACCESS_ADMIN, true);
+
+        return $this->render('@KaikmediaPagesModule/Import/index.html.twig', [
+
+        ]);
+    }
+}
